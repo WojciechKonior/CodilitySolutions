ac3ebaf1b364497460c2fc2f825a6bf4a08d6927
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2023-08-08 14:14:36 +0000

    tapeEquilibrium example added2

--- tapeEquilibrium.cpp
+++ tapeEquilibrium.cpp
@@ -6,18 +6,19 @@ using namespace std;
 int solution(vector<int> &A) {
     // Implement your solution here
     unsigned size = A.size();
-    long long diff = 0;
+    vector<long long> sums;
+    long long sum = 0;
+    for(auto a : A){
+        sum += a;
+        sums.push_back(sum);
+    }
+
+    long long diff = 9223372036854775800;
     long long tmp = 0;
 
     for(unsigned i = 1; i<size; i++){
-        long long right = 0;
-        long long left = 0;
-        for(unsigned j = 0; j<i; j++) left += A[j];
-        for(unsigned j = i; j<size; j++) right += A[j];
-
-        tmp = abs(left - right);
-
-        if(i == 1 || tmp<diff) diff = tmp;
+        tmp = abs(2*sums[i-1] - sum);
+        if(tmp<diff) diff = tmp;
     }
 
     return diff;
