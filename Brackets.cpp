d7448d08139728237a3b53b40980707cc3884071
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2023-08-14 14:40:43 +0200

    Update Brackets.cpp

--- Brackets.cpp
+++ Brackets.cpp
@@ -1,75 +1,18 @@
+#include <deque>
+#include <string>
+
+using namespace std;
+
 int solution(string &S) {
-    
-    int brackets[6] = {0, 0, 0, 0, 0, 0};
-    char last = S[S.size()-1];
+    deque<char> deq;
 
-    if(S.empty()) return 1;
-
-    if(last == '}')
-        brackets[1]++;
-    else if(last == ')')
-        brackets[3]++;
-    else if(last == ']')
-        brackets[5]++;
-    else return 0;
-
-    for(int i = 0; i<int(S.size()-1); i++)
+    for(char& c : S)
     {
-        switch(S[i]){
-            case '{':
-                if(S[i+1]==')' || S[i+1]==']') return 0;
-                brackets[0]++;
-                break;
-            case '}':
-                brackets[1]++;
-                break;
-            case '(':
-                if(S[i+1]=='}' || S[i+1]==']') return 0;
-                brackets[2]++;
-                break;
-            case ')':
-                brackets[3]++;
-                break;
-            case '[':
-                if(S[i+1]==')' || S[i+1]=='}') return 0;
-                brackets[4]++;
-                break;
-            case ']':
-                brackets[5]++;
-                break;
-            default:
-                break;          
+        if(!deq.empty() && ((c == deq.back() + 2) || (c == deq.back() + 1))) 
+            deq.pop_back();
+        else {
+            deq.push_back(c);
         }
     }
-
-    if(
-        brackets[0]!=brackets[1] || 
-        brackets[2]!=brackets[3] || 
-        brackets[4]!=brackets[5]
-    ) 
-    {
-        // for(int i = 0; i<6; i++) cout << brackets[i] << " ";
-        // cout << endl;
-        return 0;
-    }
-    else{
-        int found;
-        auto NPOS = int(string::npos);
-        do
-        {
-            found = S.find("()");
-            if(found==NPOS){
-                found = S.find("[]");
-                if(found==NPOS){
-                    found = S.find("{}");
-                }
-            }
-
-            if(found != NPOS){
-                S.erase(found,2);
-            }
-        }while(found != NPOS);
-    }
-    
-    return S.empty()?1:0;
+    return deq.empty() ? 1 : 0;
 }
