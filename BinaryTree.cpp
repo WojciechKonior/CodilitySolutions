bf4498a5d59bd73f76f00b4e00327b74390f5f34
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2023-08-20 00:11:47 +0200

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <set>
+#include <string>
 
 using namespace std;
 
@@ -10,7 +11,26 @@ struct tree
     tree *r;
 };
 
+string choose(tree **T)
+{
+    string x = to_string((*T)->x);
+    if((*T)->l == nullptr && (*T)->r == nullptr){
+        *T = nullptr;
+        return x;
+    }
+    else if(){
+    }
+    else if(){
+    }
+    return "";
+}
+
 int solution(tree *T)
 {
-    return 0;
+    set<string> st;
+    while(T!=nullptr){
+        string number = choose(T);
+        if(number.length()==3) st.insert(number);
+    }
+    return st.size();
 }
