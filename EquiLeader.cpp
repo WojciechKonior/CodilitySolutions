a49f5cf6aeb91569ad65d02f43e1c2d2bc797d4c
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2023-08-24 13:39:03 +0200

    Update EquiLeader.cpp

--- EquiLeader.cpp
+++ EquiLeader.cpp
@@ -1,49 +1,7 @@
 #include <vector>
 #include <unordered_map>
 
-using namespace std;
-
-int computeEquiLeader(unordered_map<int, unsigned>& mp, unsigned size){
-    for(auto& m : mp){
-        if(m.second > size) {
-            return m.first;
-        }
-    }
-    return -11e8;
-}
-
-int solution(vector<int> &A) {
-
-    unsigned size = A.size();
-    
-    int eq_lead = 0;
-    unordered_map<int, unsigned> mp1;
-    unordered_map<int, unsigned> mp2;
-
-    for(unsigned i = 0; i<size; i++){
-        mp2[A[i]]++;
-    }
-
-    for(unsigned i = 0; i<size-1; i++){
-        mp1[A[i]]++;
-        mp2[A[i]]--;
-
-        unsigned size1 = (i+1)/2;
-        unsigned size2 = (size-i-1)/2;
-
-        int lead1 = computeEquiLeader(mp1, size1);
-        int lead2 = computeEquiLeader(mp2, size2);
-
-        if(lead1 > -11e8 && lead1 == lead2)
-            eq_lead++;
-    }
-
-    return eq_lead;
-}
-
-
-#include <vector>
-#include <unordered_map>
+#define INF 2e9
 
 using namespace std;
 
@@ -53,33 +11,44 @@ int findEquiLeader(unordered_map<int, unsigned>& mp, unsigned& size){
             return m.first;
         }
     }
-    return -11e8;
+    return INF;
 }
 
-// int solution(vector<int> &A) {
+int solution(vector<int> &A) {
 
-//     unsigned size = A.size();
+    int eq_lead = 0;
+    unsigned size = A.size();
+    unordered_map<int, unsigned> mp2;
     
-//     int eq_lead = 0;
-//     unordered_map<int, unsigned> mp1;
-//     unordered_map<int, unsigned> mp2;
+    for(unsigned i = 0; i<size; i++){
+        mp2[A[i]]++;
+    }
 
-//     for(unsigned i = 0; i<size; i++){
-//         mp2[A[i]]++;
-//     }
+    unsigned s = mp2.size();
+    int ld = findEquiLeader(mp2, s);
+    if(ld==INF) return eq_lead;
 
-//     for(unsigned i = 0; i<size-1; i++){
-//         mp1[A[i]]++;
-//         mp2[A[i]]--;
+    unordered_map<int, unsigned> mp1;
 
-//         unsigned size1 = (i+1)/2;
-//         unsigned size2 = (size-i-1)/2;
+    for(unsigned i = 0; i<size-1; i++){
+        mp1[A[i]]++;
+        mp2[A[i]]--;
 
-//         int lead2 = findEquiLeader(mp2, size2);
+        unsigned size1 = (i+1)/2;
+        unsigned size2 = (size-i-1)/2;
+        
 
-//         if(lead2 > -11e8 && mp1[lead2] > size1)
-//             eq_lead++;
-//     }
+        if(mp2.size() < mp1.size()){
+            int lead2 = findEquiLeader(mp2, size2);
+            if(lead2 != INF && mp1[lead2] > size1)
+                eq_lead++;
+        }
+        else {
+            int lead1 = findEquiLeader(mp1, size1);
+            if(lead1 != INF && mp2[lead1] > size2)
+                eq_lead++;
+        }
+    }
 
-//     return eq_lead;
-// }
+    return eq_lead;
+}
