e6bf831b0707ba95fffa3765b491daa94e804260
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2024-01-30 16:46:40 +0100

    Update OddOccurencesInArray.cpp

--- cpp/OddOccurencesInArray.cpp
+++ cpp/OddOccurencesInArray.cpp
@@ -4,13 +4,11 @@
 using namespace std;
 
 int solution(vector<int> &A) {
-    unsigned int size = A.size();
-    unsigned long long int sum = 0;
-    
-    for (unsigned int i=0; i<size; i++) {
-        // A xor B xor A = B
-        sum ^= A[i];
+    int result = 0;
+    for(auto &a : A)
+    {
+        result ^= a;
     }
-    
-    return sum;
+
+    return result;
 }
