4b7c5b199d20dc7f910fc30d050c2b342db2f3ab
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2024-02-02 18:05:25 +0100

    Update TapeEquilibrium.cpp

--- cpp/TapeEquilibrium.cpp
+++ cpp/TapeEquilibrium.cpp
@@ -1,25 +1,22 @@
+#include <limits.h>
 #include <math.h>
-#include <vector>
-
-using namespace std;
 
 int solution(vector<int> &A) {
     // Implement your solution here
-    unsigned size = A.size();
-    vector<long long> sums;
-    long long sum = 0;
-    for(auto a : A){
-        sum += a;
-        sums.push_back(sum);
+    int first = 0;
+    int second = 0;
+    int min_diff = INT_MAX;
+    for(auto &a : A)
+        second += a;
+
+    for(unsigned i = 1; i<A.size(); i++)
+    {
+        first += A[i-1];
+        second -= A[i-1];
+        int diff = abs(first - second);
+        if (diff < min_diff) 
+            min_diff = diff;
     }
 
-    long long diff = 9223372036854775800;
-    long long tmp = 0;
-
-    for(unsigned i = 1; i<size; i++){
-        tmp = abs(2*sums[i-1] - sum);
-        if(tmp<diff) diff = tmp;
-    }
-
-    return diff;
+    return min_diff;
 }
