d5b9563ccdce0ac8d0ae847251ca9c9209689f00
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2024-02-02 18:46:22 +0100

    Update TapeEquilibrium.py

--- python/TapeEquilibrium.py
+++ python/TapeEquilibrium.py
@@ -1,19 +1,21 @@
-
-
 def solution(A):
+    if(len(A)==0 or len(A)==1):
+        return 0
+
     rhs = 0
     lhs = 0
 
     for a in A:
         rhs += a
 
+    rhs -= A[0]
+    lhs += A[0]
     min_diff = abs(lhs - rhs)
     
-    for i in range(1, len(A)-1):
+    for i in range(2, len(A)):
         lhs += A[i-1]
         rhs -= A[i-1]
         diff = abs(lhs - rhs)
         min_diff = min(min_diff, diff)
 
     return min_diff
-
