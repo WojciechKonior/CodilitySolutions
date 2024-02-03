8ca178f89bc33f806f1a8608272a425aa02a6f70
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2024-02-03 19:33:06 +0100

    Update FrogRiverOne.cpp

--- cpp/FrogRiverOne.cpp
+++ cpp/FrogRiverOne.cpp
@@ -3,13 +3,31 @@
 
 using namespace std;
 
-int solution(int X, vector<int> &A) {
+// int solution(int X, vector<int> &A) {
 
-    set<int>  positions;
+//     set<int>  positions;
+//     for(unsigned i = 0; i<A.size(); i++)
+//     {
+//         positions.insert(A[i]);
+//         if(positions.size()==unsigned(X)) return i;
+//     }
+//     return -1;
+// }
+
+int solution(int X, vector<int> &A) {
+    vector<int> vec(X, 0);
+    int cnt = 0;
     for(unsigned i = 0; i<A.size(); i++)
     {
-        positions.insert(A[i]);
-        if(positions.size()==unsigned(X)) return i;
+        if(A[i]<=X && A[i]>0)
+        {
+            vec[A[i]-1]++;
+            if(vec[A[i]-1] == 1) 
+            {
+                cnt++;
+                if (cnt==X) return int(i);
+            }
+        }
     }
     return -1;
 }
