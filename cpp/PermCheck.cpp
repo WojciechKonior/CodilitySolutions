25095bfb6d1476ee0d4e299ee5416cda5de4e80e
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2024-02-04 14:28:08 +0100

    Update PermCheck.cpp

--- cpp/PermCheck.cpp
+++ cpp/PermCheck.cpp
@@ -3,17 +3,30 @@
 
 using namespace std;
 
+// int solution(vector<int> &A) {
+//     set<int> perm;
+//     for(auto a : A)
+//         perm.insert(a);
+
+//     unsigned max = *perm.rbegin();
+//     unsigned size = A.size();
+//     unsigned psize = perm.size();
+
+//     if(max == size && size == psize) 
+//         return 1;
+
+//     return 0;
+// }
+
 int solution(vector<int> &A) {
-    set<int> perm;
-    for(auto a : A)
-        perm.insert(a);
-
-    unsigned max = *perm.rbegin();
-    unsigned size = A.size();
-    unsigned psize = perm.size();
-
-    if(max == size && size == psize) 
-        return 1;
-
-    return 0;
+    int S = A.size();
+    vector<int> B(S, 0);
+    for(auto &a : A)
+    {
+        if(a>S) 
+            return 0;
+        if(++B[a-1]>1)
+            return 0;
+    }
+    return 1;
 }
