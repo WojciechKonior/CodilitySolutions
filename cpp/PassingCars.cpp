35569170c0f6f297d756afc9c01d237da3cf35c7
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2024-02-09 12:27:41 +0100

    Update PassingCars.cpp

--- cpp/PassingCars.cpp
+++ cpp/PassingCars.cpp
@@ -3,21 +3,37 @@
 
 using namespace std;
 
-int solution(vector<int> &A) {
-    int cnt=0, e_cnt=0;
+// int solution(vector<int> &A) {
+//     int cnt=0, e_cnt=0;
     
-    for(const auto& a : A)
+//     for(const auto& a : A)
+//     {
+//         if(a==0){
+//             e_cnt++;
+//         }
+//         else {
+//             cnt += e_cnt;
+//             if(cnt>1e9){
+//                 return -1;
+//             }
+//         }
+//     }
+
+//     return cnt;
+// }
+
+int solution(vector<int> &A) {
+
+    unsigned long long pairs = 0;
+    unsigned long long e = 0;
+
+    for(auto &a : A)
     {
-        if(a==0){
-            e_cnt++;
-        }
-        else {
-            cnt += e_cnt;
-            if(cnt>1e9){
-                return -1;
-            }
-        }
+        if(a==0) 
+            e++;
+        else
+            pairs += e;
     }
 
-    return cnt;
+    return pairs>1e9 ? -1 : int(pairs);
 }
