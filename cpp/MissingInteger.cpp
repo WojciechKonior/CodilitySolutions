4ea8b5591a45d050add58dc7081a407b292ad44d
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2024-02-09 11:39:23 +0100

    Update MissingInteger.cpp

--- cpp/MissingInteger.cpp
+++ cpp/MissingInteger.cpp
@@ -4,15 +4,32 @@
 
 using namespace std;
 
+// int solution(vector<int> &A) {
+//     set<int> st;
+  
+//     for(auto& a: A) 
+//       st.insert(a);
+  
+//     for(int i = 1; ; i++) 
+//       if(st.find(i)==st.end()) 
+//         return i;
+
+//     return 1;
+// }
+
+
 int solution(vector<int> &A) {
-    set<int> st;
-  
-    for(auto& a: A) 
-      st.insert(a);
-  
-    for(int i = 1; ; i++) 
-      if(st.find(i)==st.end()) 
-        return i;
+    vector<bool> B(1e6, false);
+
+    for(auto &a : A)
+    {
+        if(a<0) continue;
+        B[a] = true;
+    }
+
+    for(unsigned i = 1; i<B.size(); i++)
+        if (B[i]==false)
+            return int(i);
 
     return 1;
 }
