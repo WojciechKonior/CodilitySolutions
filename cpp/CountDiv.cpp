c08d971ac99c814ed4235e7ae54f6aa3e5eca6a1
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2024-02-14 18:46:40 +0100

    Update CountDiv.cpp

--- cpp/CountDiv.cpp
+++ cpp/CountDiv.cpp
@@ -1,10 +1,13 @@
 int solution(int A, int B, int K) {
+    if(K==1) return B-A;
     int cnt = 0;
-
-    for(int i = A; i<=B; i++)
+    for(int i = A/K; i<=B; i++)
     {
-        if(i%K==0) cnt++;
+        int n = i*K;
+        if(n<A) continue;
+        if(n>B) break;
+        cnt++;
+        
     }
-
     return cnt;
 }
