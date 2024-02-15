fc65022848c34e85f00bea314854f114bb31fefd
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2024-02-15 17:26:40 +0100

    Update GenomicRangeQuery.py

--- python/GenomicRangeQuery.py
+++ python/GenomicRangeQuery.py
@@ -5,10 +5,24 @@ mp = {
     'T': 4
 }
 
+# def solution(S, P, Q):
+#     res = [0 for i in range(0, len(P))]
+
+#     for i in range(0, len(P)):
+#         res[i] = mp[min(S[P[i]:Q[i]+1])]
+
+#     return res
+
 def solution(S, P, Q):
     res = [0 for i in range(0, len(P))]
 
     for i in range(0, len(P)):
-        res[i] = mp[min(S[P[i]:Q[i]+1])]
+        min_mp = 'T'
+        for j in range(P[i], Q[i]+1):
+            min_mp = min(min_mp, S[j])
+            if min_mp == 'A': break
+
+        res[i] = mp[min_mp]
+        min_mp = 'T'
 
     return res
