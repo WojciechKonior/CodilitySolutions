a62656c6ded8f7cd8ca02151b3e7a9717fafda30
VoyTec <78218606+WojciechKonior@users.noreply.github.com>
2024-02-16 21:07:54 +0100

    Update GenomicRangeQuery.py

--- python/GenomicRangeQuery.py
+++ python/GenomicRangeQuery.py
@@ -1,9 +1,9 @@
-mp = {
-    'A': 1,
-    'C': 2,
-    'G': 3,
-    'T': 4
-}
+# mp = {
+#     'A': 1,
+#     'C': 2,
+#     'G': 3,
+#     'T': 4
+# }
 
 # def solution(S, P, Q):
 #     res = [0 for i in range(0, len(P))]
@@ -13,16 +13,40 @@ mp = {
 
 #     return res
 
+# def solution(S, P, Q):
+#     res = [0 for i in range(0, len(P))]
+
+#     for i in range(0, len(P)):
+#         min_mp = 'T'
+#         for j in range(P[i], Q[i]+1):
+#             min_mp = min(min_mp, S[j])
+#             if min_mp == 'A': break
+
+#         res[i] = mp[min_mp]
+#         min_mp = 'T'
+
+#     return res
+
 def solution(S, P, Q):
-    res = [0 for i in range(0, len(P))]
+    mp = {
+        'A': 1,
+        'C': 2,
+        'G': 3,
+        'T': 4
+    }
+    res = []
+    M = len(P)
+    for i in range(0, M):
+        min_mp = S[P[i]]
+        if min_mp!='A':
+            for j in range(P[i], Q[i]+1):
+                if S[j]=='A': 
+                    min_mp = S[j]
+                    break
+                elif S[j]==min_mp: continue
+                
+                min_mp = min(min_mp, S[j])
 
-    for i in range(0, len(P)):
-        min_mp = 'T'
-        for j in range(P[i], Q[i]+1):
-            min_mp = min(min_mp, S[j])
-            if min_mp == 'A': break
-
-        res[i] = mp[min_mp]
-        min_mp = 'T'
+        res.append(mp[min_mp])
 
     return res
